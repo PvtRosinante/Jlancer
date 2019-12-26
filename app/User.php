6c34c7180eee96b441384ad089dc034e3a7774b4
PvtRosinante <jlancer492@gmail.com>
2019-12-26 14:30:26 +0700

    sec

---
+++ app/User.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App;
+
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'skill'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function contact()
+    {
+        return $this->hasMany(Contact::class);
+    }
+    
+    public function award()
+    {
+        return $this->hasMany(Award::class);
+    }
+    
+    public function education()
+    {
+        return $this->hasMany(Education::class);
+    }
+
+    public function experience()
+    {
+        return $this->hasMany(Experience::class);
+    }
+
+    public function other()
+    {
+        return $this->hasMany(Other::class);
+    }
+
+    public function project()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public function summary()
+    {
+        return $this->hasMany(Summary::class);
+    }
+}
