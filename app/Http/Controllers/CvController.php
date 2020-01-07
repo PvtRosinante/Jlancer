2be6ace025c426877c5cac09922993ec898dfa7d
PvtRosinante <jlancer492@gmail.com>
2020-01-07 22:18:57 +0700

    updatedelete cv

--- app/Http/Controllers/CvController.php
+++ app/Http/Controllers/CvController.php
@@ -72,6 +72,99 @@ class CvController extends Controller
         //
     }
 
+    public function updatecontact(Request $request, $id)
+    {
+        $cnt = Contact::find($id);
+
+        $cnt->user_id = Auth::user()->id;
+        $cnt->phone = $request->phone;
+        $cnt->facebook = $request->facebook;
+        $cnt->twitter = $request->twitter;
+        $cnt->instagram = $request->instagram;
+        $cnt->update();
+
+        return response()->json(['Messages'=> 'Successfully updating the Contact']);
+    }
+
+    public function updateaward(Request $request, $id)
+    {
+        $aw = Award::find($id);
+
+        $aw->user_id = Auth::user()->id;
+        $aw->award = $request->award;
+        $aw->update();
+
+        return response()->json(['Messages'=> 'Successfully updating the Award']);
+    }
+
+    public function updateother(Request $request, $id)
+    {
+        $oth = Other::find($id);
+
+        $oth->user_id = Auth::user()->id;
+        $oth->birth = $request->birth;
+        $oth->skill = $request->skill;
+        $oth->language = $request->language;
+        $oth->interest = $request->interest;
+        $oth->other_info = $request->other_info;
+        $oth->update();
+
+        return response()->json(['Messages'=> 'Successfully updating the Other']);
+    }
+
+    public function updatesummary(Request $request, $id)
+    {
+        $sum = Summary::find($id);
+
+        $sum->user_id = Auth::user()->id;
+        $sum->summary = $request->summary;
+        $sum->update();
+
+        return response()->json(['Messages'=> 'Successfully updating the Summary']);
+    }
+
+    public function updateeducation(Request $request, $id)
+    {
+        $edu = Education::find($id);
+
+        $edu->user_id = Auth::user()->id;
+        $edu->agency = $request->agency;
+        $edu->time = $request->time;
+        $edu->study_prog = $request->study_prog;
+        $edu->description = $request->description;
+        $edu->update();
+
+        return response()->json(['Messages'=> 'Successfully updating the Education']);
+    }
+
+    public function updateexperience(Request $request, $id)
+    {
+        $exp = Experience::find($id);
+
+        $exp->user_id = Auth::user()->id;
+        $exp->experience = $request->experience;
+        $exp->time = $request->time;
+        $exp->sub_exp = $request->sub_exp;
+        $exp->description = $request->description;
+        $exp->update();
+
+        return response()->json(['Messages'=> 'Successfully updating the Experience']);
+    }
+
+    public function updateproject(Request $request, $id)
+    {
+        $prj = Project::find($id);
+
+        $prj->user_id = Auth::user()->id;
+        $prj->project = $request->project;
+        $prj->time = $request->time;
+        $prj->sub_project = $request->sub_project;
+        $prj->description = $request->description;
+        $prj->update();
+
+        return response()->json(['Messages'=> 'Successfully updating the Project']);
+    }
+
     /**
      * Update the specified resource in storage.
      *
@@ -79,9 +172,72 @@ class CvController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function updatecv(Request $request, $id)
     {
-        //
+        $cnt = Contact::find($id);
+
+        $cnt->user_id = Auth::user()->id;
+        $cnt->phone = $request->phone;
+        $cnt->facebook = $request->facebook;
+        $cnt->twitter = $request->twitter;
+        $cnt->instagram = $request->instagram;
+        $cnt->update();
+
+
+        $aw = Award::find($id);
+
+        $aw->user_id = Auth::user()->id;
+        $aw->award = $request->award;
+        $aw->update();
+
+
+        $oth = Other::find($id);
+
+        $oth->user_id = Auth::user()->id;
+        $oth->birth = $request->birth;
+        $oth->skill = $request->skill;
+        $oth->language = $request->language;
+        $oth->interest = $request->interest;
+        $oth->other_info = $request->other_info;
+        $oth->update();
+
+
+        $sum = Summary::find($id);
+
+        $sum->user_id = Auth::user()->id;
+        $sum->summary = $request->summary;
+        $sum->update();
+
+        $edu = Education::find($id);
+
+        $edu->user_id = Auth::user()->id;
+        $edu->agency = $request->agency;
+        $edu->time = $request->time;
+        $edu->study_prog = $request->study_prog;
+        $edu->description = $request->description;
+        $edu->update();
+
+
+        $exp = Experience::find($id);
+
+        $exp->user_id = Auth::user()->id;
+        $exp->experience = $request->experience;
+        $exp->time = $request->time;
+        $exp->sub_exp = $request->sub_exp;
+        $exp->description = $request->description;
+        $exp->update();
+
+
+        $prj = Project::find($id);
+
+        $prj->user_id = Auth::user()->id;
+        $prj->project = $request->project;
+        $prj->time = $request->time;
+        $prj->sub_project = $request->sub_project;
+        $prj->description = $request->description;
+        $prj->update();
+
+        return response()->json(['Messages' => 'CV was successfully updated!']);
     }
 
     /**
@@ -161,5 +317,61 @@ class CvController extends Controller
         $prj->save();
 
         return response()->json(['Messages' => 'CV was successfully inserted!']);
+    }
+
+    public function delcontact($id)
+    {
+        $cnt = Contact::find($id);
+        $cnt->delete();
+
+        return response()->json(['Messages' => 'CV was successfully deleted!']);
+    }
+    
+    public function delaward($id)
+    {
+        $aw = Award::find($id);
+        $aw->delete();
+
+        return response()->json(['Messages' => 'CV was successfully deleted!']);
+    }
+
+    public function delother($id)
+    {
+        $oth = Other::find($id);
+        $oth->delete();
+
+        return response()->json(['Messages' => 'CV was successfully deleted!']);
+    }
+
+    public function delsummary($id)
+    {
+        $sum = Summary::find($id);
+        $sum->delete();
+
+        return response()->json(['Messages' => 'CV was successfully deleted!']);
+    }
+
+    public function deleducation($id)
+    {
+        $edu = Education::find($id);
+        $edu->delete();
+
+        return response()->json(['Messages' => 'CV was successfully deleted!']);
+    }
+
+    public function delexperience($id)
+    {
+        $exp = Experience::find($id);
+        $exp->delete();
+
+        return response()->json(['Messages' => 'CV was successfully deleted!']);
+    }
+
+    public function delproject($id)
+    {
+        $prj = Project::find($id);
+        $prj->delete();
+
+        return response()->json(['Messages' => 'CV was successfully deleted!']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,17 +30,31 @@ Route::group([ 'prefix' => 'auth'], function (){
 
 Route::group(['middleware' => 'auth:api'], function() {
     Route::post('/cv', 'CvController@insertcv');
+    Route::post('/cv-update/contact/{id}', 'CvController@updatecontact');
+    Route::post('/cv-update/award/{id}', 'CvController@updateaward');
+    Route::post('/cv-update/other/{id}', 'CvController@updateother');
+    Route::post('/cv-update/summary/{id}', 'CvController@updatesummary');
+    Route::post('/cv-update/education/{id}', 'CvController@updateeducation');
+    Route::post('/cv-update/experience/{id}', 'CvController@updateexperience');
+    Route::post('/cv-update/project/{id}', 'CvController@updateproject');
+    Route::delete('/cv-del/contact/{id}', 'CvController@delcontact');
+    Route::delete('/cv-del/award/{id}', 'CvController@delaward');
+    Route::delete('/cv-del/other/{id}', 'CvController@delother');
+    Route::delete('/cv-del/summary/{id}', 'CvController@delsummary');
+    Route::delete('/cv-del/education/{id}', 'CvController@deleducation');
+    Route::delete('/cv-del/experience/{id}', 'CvController@delexperience');
+    Route::delete('/cv-del/project/{id}', 'CvController@delproject');
 });
 
 Route::get('vacancy', 'VacancyController@index');
 Route::post('vacancy', 'VacancyController@create');
-Route::post('/vacancy/{id}', 'VacancyController@update');
-Route::delete('/vacancy/{id}', 'VacancyController@delete');
+Route::post('/vacancy-update/{id}', 'VacancyController@update');
+Route::delete('/vacancy-del/{id}', 'VacancyController@delete');
 
 Route::get('skill', 'SkillController@index');
 Route::post('skill', 'SkillController@create');
-Route::post('/skill/{id}', 'SkillController@update');
-Route::delete('/skill/{id}', 'SkillController@delete');
+Route::post('/skill-update/{id}', 'SkillController@update');
+Route::delete('/skill-del/{id}', 'SkillController@delete');
 
 Route::get('/cv', 'CvController@index');
 Route::get('/cv/{id}', 'CvController@show');
